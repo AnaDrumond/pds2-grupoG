699e2205551c32437c924298a4f2ab1f4ab6c78d
AnaDrumond <analuizagd05@gmail.com>
2024-04-15 21:41:22 -0300

    Adicionando o arquivos para a função de renovação de empréstimos.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "pagamento.hpp"
+#include "renovacao.hpp"
 
 using namespace std;
 
@@ -12,5 +13,19 @@ int main() {
     Multa multaSelecionada = selecionarMulta(indiceMulta);
     realizarPagamento(multaSelecionada);
 
+    std::vector<Livro> acervo = {
+        {"A Revolucao dos Bichos", "George Orwell", 14, true},
+        {"O Senhor dos Aneis", "J.R.R. Tolkien", 7, false},
+        {"1984", "George Orwell", 21, true}
+    };
+
+    listarLivrosDisponiveis(acervo);
+
+    std::string titulo;
+    std::cout << "\nDigite o titulo do livro que deseja renovar o emprestimo: ";
+    std::getline(std::cin, titulo);
+
+    renovarEmprestimo(acervo, titulo);
+    
     return 0;
 }
