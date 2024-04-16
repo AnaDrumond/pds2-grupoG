311064966b43ba787a183d7c3a1632d185020b5c
larastrutz <162838377+larastrutz@users.noreply.github.com>
2024-04-16 09:45:47 -0300

    Update

--- src/biblioteca.cpp
+++ src/biblioteca.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 namespace Biblioteca {
     vector<Livro> listarLivrosDisponiveis() {
-        return {{"Livro 1", "Autor 1", true}, {"Livro 2", "Autor 2", false}, {"Livro 3", "Autor 3", true}};
+        return {{"Livro 1", "Autor 1", true}, {"Livro 2", "Autor 2", true}, {"Livro 3", "Autor 3", true}};
     }
 
     bool reservarLivro(const string& titulo) {
